c0ebc127af24dbf309a3cf64bcd609dc0f958931
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-04 18:20:46 +0530

    refractor: Cleanup the useless inbuilt code.

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type { Node } from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+const App: () => Node = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello world!</Text>
+    </SafeAreaView >
+  );
+};
+
+export default App;
