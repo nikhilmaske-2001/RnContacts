f27b0ac43ac537c6aadd48a0f6994fead01d397f
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-05 10:13:34 +0530

    navigation: Create AuthNavigator structure and its components.

---
+++ src/navigations/AuthNavigator.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Text, View } from 'react-native';
+
+const Login = () => {
+    return (
+        <Viewiew>
+            <Text>Hi, I am from Contacts</Text>
+        </View >
+    );
+}
+
+const SignUp = () => {
+    return (
+        <View>
+            <Text>Hi, I am from ContactDetail</Text>
+        </View >
+    );
+}
+
+
+const AuthNavigator = () => {
+    const HomeStack = createStackNavigator();
+    return (
+        <AuthStack.Navigator>
+            <AuthStack.Screen name="Login" component={Login}></AuthStack.Screen>
+            <AuthStack.Screen name="Register" component={SignUp}></AuthStack.Screen>
+        </AuthStack.Navigator >
+    );
+}
+
+export default AuthNavigator;
