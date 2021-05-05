c7c1214a435d48bb59672fc47ab401686284a29a
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-05 10:07:42 +0530

    navigation: Create homeNavigators structure and its components.

---
+++ src/navigations/HomeNavigator.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Text, View } from 'react-native';
+
+const Contacts = () => {
+    return (
+        <Viewiew>
+            <Text>Hi, I am from Contacts</Text>
+        </View >
+    );
+}
+
+const ContactDetail = () => {
+    return (
+        <View>
+            <Text>Hi, I am from ContactDetail</Text>
+        </View >
+    );
+}
+
+const CreateContact = () => {
+    return (
+        <View>
+            <Text>Hi, I am from CreateContact</Text>
+        </View >
+    );
+}
+
+const Settings = () => {
+    return (
+        <View>
+            <Text>Hi, I am from Settings</Text>
+        </View >
+    );
+}
+
+const HomeNavigator = () => {
+    const HomeStack = createStackNavigator();
+    return (
+        <HomeStack.Navigator>
+            <HomeStack.Screen name="Contacts" component={Contacts}></HomeStack.Screen>
+            <HomeStack.Screen name="Contact Details" component={ContactDetail}></HomeStack.Screen>
+            <HomeStack.Screen name="Create Contact" component={CreateContact}></HomeStack.Screen>
+            <HomeStack.Screen name="Settings" component={Settings}></HomeStack.Screen>
+        </HomeStack.Navigator >
+    );
+}
+
+export default HomeNavigator;
