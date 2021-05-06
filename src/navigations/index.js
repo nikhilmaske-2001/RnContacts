9c94bfd455532b98a776a7aeb3b2d92ae82c6182
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-06 15:22:42 +0530

    install react drawer navigator dependencies

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Text } from "react-native";
 import AuthNavigator from './AuthNavigator';
+import DrawerNavigator from './DrawerNavigator';
 
 const AppNavContainer = () => {
     return (
         <NavigationContainer>
-            <Text>Hello world! This is index.js</Text>
-            <AuthNavigator />
+            {/* <Text>Hello world! This is index.js</Text> */}
+            {/* <AuthNavigator /> */}
+            <DrawerNavigator />
         </NavigationContainer >
     );
 }
