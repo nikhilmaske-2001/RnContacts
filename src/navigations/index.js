d34ad29f267813bf71c66d0e9e265fc0eeaf2970
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-06 15:40:25 +0530

    ifelse: Add contional statements for multiple screens.

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { Text } from "react-native";
 import AuthNavigator from './AuthNavigator';
 import DrawerNavigator from './DrawerNavigator';
 
 const AppNavContainer = () => {
+    const isLoggedin = true;
     return (
         <NavigationContainer>
-            {/* <Text>Hello world! This is index.js</Text> */}
-            {/* <AuthNavigator /> */}
-            <DrawerNavigator />
+            {isLoggedin ? <DrawerNavigator /> : <AuthNavigator />}
         </NavigationContainer >
     );
 }
